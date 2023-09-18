d75b017f1725e7e216b9a5bcd5dd7392d0060c6e
Lucas Diniz <53830752+Akssasori@users.noreply.github.com>
2023-09-17 23:07:41 -0300

    created repository and static method create user

--- src/useCases/create-user/create-user.useCases.ts
+++ src/useCases/create-user/create-user.useCases.ts
@@ -1,3 +1,5 @@
+import { User } from "../../entities/user.entity";
+import { UserRepository } from "../../repositories/user.repostory"
 
 type UserRequest = {
     name:string,
@@ -6,8 +8,23 @@ type UserRequest = {
 }
 
 export class CreateUserCase {
-
     async execute (data: UserRequest) {
+        const userRepository = new UserRepository();
 
+        if (!data.username && !data.password) {
+            throw new Error('Username/passwprd is required.');
+        }
+
+        const existUser = await userRepository.findByUsername(data.username);
+
+        if(existUser) {
+            throw new Error('Username already exists')
+        }
+
+        const user = User.create(data);
+
+        const userCreated = await userRepository.save(user);
+        
+        return userCreated;
     }
 }
